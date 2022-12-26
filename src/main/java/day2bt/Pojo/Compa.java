d1a5da439d0a5649e12360b3bf772d8d7b090d47
phannhattai9981 <you@example.com>
2022-12-26 18:04:43 +0700

    new

---
+++ src/main/java/day2bt/Pojo/Compa.java
@@ -0,0 +1,7 @@
+package day2bt.Pojo;
+
+public interface Compa {
+    void painting(long fromId, String fromName, double radius);
+
+    Student getStudent(long studentId);
+}
---
+++ src/main/java/day2bt/Pojo/StudentService.java
@@ -0,0 +1,9 @@
+package day2bt.Pojo;
+
+public interface StudentService{
+        void insert(Student student);
+        void update(Student student);
+        Student find(long id);
+}
+
+

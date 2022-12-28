1639e4758ef3c7df9e731444034b07569dc21504
phannhattai9981 <121222709+phannhattai9981@users.noreply.github.com>
2022-12-28 20:03:12 +0700

    Delete src/main/java/day2bt directory

--- src/main/java/day2bt/Pojo/Compa.java
+++
@@ -1,7 +0,0 @@
-package day2bt.Pojo;
-
-public interface Compa {
-    void painting(long fromId, String fromName, double radius);
-
-    Student getStudent(long studentId);
-}
--- src/main/java/day2bt/Pojo/StudentService.java
+++
@@ -1,9 +0,0 @@
-package day2bt.Pojo;
-
-public interface StudentService{
-        void insert(Student student);
-        void update(Student student);
-        Student find(long id);
-}
-
-
